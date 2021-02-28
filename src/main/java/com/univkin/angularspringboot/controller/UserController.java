b7cbb5909cd4ef4dc2355439d2ef3dfd2a3cb2da
LELO <nyaka.lelo@econocom.com>
2021-02-28 01:28:16 +0100

    Ajout jackson dependency

--- src/main/java/com/univkin/angularspringboot/controller/UserController.java
+++ src/main/java/com/univkin/angularspringboot/controller/UserController.java
@@ -5,11 +5,12 @@ import javax.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.univkin.angularspringboot.entite.UserEntite;
 import com.univkin.angularspringboot.request.UserRequest;
@@ -22,26 +23,8 @@ public class UserController {
 	@Autowired
 	UserService userService;
 
-	/*
-	 * @PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE,
-	 * MediaType.APPLICATION_JSON_VALUE }, produces = {
-	 * MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }) public
-	 * ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest
-	 * userRequest) throws Exception {
-	 * 
-	 * ModelMapper modelMapper = new ModelMapper(); UserEntite userDto =
-	 * modelMapper.map(userRequest, UserEntite.class);
-	 * 
-	 * UserEntite createUser = userService.createUser(userDto);
-	 * 
-	 * UserResponse userResponse = modelMapper.map(createUser, UserResponse.class);
-	 * 
-	 * return new ResponseEntity<UserResponse>(userResponse, HttpStatus.CREATED);
-	 * 
-	 * }
-	 */
-
-	@RequestMapping(method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+	@PostMapping(consumes = { MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE }, produces = {
+			MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE })
 	public ResponseEntity<UserResponse> createUser(@RequestBody @Valid UserRequest userRequest) throws Exception {
 
 		ModelMapper modelMapper = new ModelMapper();
@@ -54,4 +37,21 @@ public class UserController {
 		return new ResponseEntity<UserResponse>(userResponse, HttpStatus.CREATED);
 
 	}
+
+	/*
+	 * @RequestMapping(method = RequestMethod.POST, produces = "application/json",
+	 * consumes = "application/json") public ResponseEntity<UserResponse>
+	 * createUser(@RequestBody @Valid UserRequest userRequest) throws Exception {
+	 * 
+	 * ModelMapper modelMapper = new ModelMapper(); UserEntite userDto =
+	 * modelMapper.map(userRequest, UserEntite.class);
+	 * 
+	 * UserEntite createUser = userService.createUser(userDto);
+	 * 
+	 * UserResponse userResponse = modelMapper.map(createUser, UserResponse.class);
+	 * 
+	 * return new ResponseEntity<UserResponse>(userResponse, HttpStatus.CREATED);
+	 * 
+	 * }
+	 */
 }
